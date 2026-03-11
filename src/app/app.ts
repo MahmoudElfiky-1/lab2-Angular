1e87010317c453586f25e1fe7324b885de37aed6
Mahmoud Ismail <mahmoudelnfiky@gmail.com>
2026-03-11 13:12:36 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Navbar } from "./components/navbar/navbar";
+import { Products } from "./components/products/products";
+import { Slider } from "./components/slider/slider";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Footer, Navbar, Products, Slider],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
